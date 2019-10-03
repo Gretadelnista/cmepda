a2cb9db3b3212139acaed949e954b3fc66cb07e5
greta del nista <gretadelnista@MacBook-Pro-di-greta.local>
2019-10-03 11:19:45 +0200

    Using action_true as store option for boolean input

--- assignment_basic_1.py
+++ assignment_basic_1.py
@@ -12,22 +12,22 @@ _DESCRIPTION = 'Measure the releative frequencies of letters in a text file'
 def process(file_path):
     logging.info('Opening file %s...', file_path)
     with open(file_path, 'r') as file_input:
-        f = file_input.read()
-        f = f.lower()
+        text = file_input.read()
+        text = text.lower()
     logging.info('Done.')
 
     # Optional skip of preamble and licence
-    if args.skip != None:
+    if args.skip == True:
         logging.info('Skipping the preamble...')
-        ID = re.compile('\*\*\*')
-        logging.debug('ID preamble-end: %s', \
-                      f[ID.search(f).start():ID.search(f, ID.search(f).end()).end()])
-        preamble_end = ID.search(f, ID.search(f).end()).end()
-        f = f[preamble_end:]              # skip preamble
+        id_ = re.compile('\*\*\*')
+        logging.debug('id_ preamble-end: %s', \
+                      text[id_.search(text).start():id_.search(text, id_.search(text).end()).end()])
+        preamble_end = id_.search(text, id_.search(text).end()).end()
+        text = text[preamble_end:]              # skip preamble
         logging.info('Done.')
         logging.info('Skipping the licence...')
-        logging.debug('Skip licence: %s', f[ID.search(f, preamble_end).start():])
-        f = f[:ID.search(f, preamble_end).start()]                 # skip licence
+        logging.debug('Skip licence: %s', text[id_.search(text, preamble_end).start():])
+        text = text[:id_.search(text, preamble_end).start()]                 # skip licence
         logging.info('Done.')
 
     # Prepare a dictionary to hold the letter frequencies, and initialize
@@ -36,7 +36,7 @@ def process(file_path):
     letter = {str(i) : 0 for i in alphabet}
     logging.debug('Dict letter: %s', letter)
     # Loop over the input data
-    for i in f:
+    for i in text:
         if i in letter:
             letter[i] += 1
 
@@ -54,7 +54,7 @@ def process(file_path):
 
 
     # Optional display of the frequences' histogram
-    if args.histogram != None:
+    if args.histogram == True:
         logging.info('Displaying the frequences\' histogram')
         plt.title('Bar plot of the frequences')
         plt.bar(x, y, tick_label=x_tick)
@@ -63,12 +63,12 @@ def process(file_path):
         logging.info('Done.')
 
     # Optional print of the statistic
-    if args.statistic != None:
+    if args.statistic == True:
         logging.info('Printing the basic statistic...')
-        n_chars = len(f)
-        n_words = len(f.split())
+        n_chars = len(text)
+        n_words = len(text.split())
         end_line = re.compile('\\n')
-        n_lines = len(end_line.findall(f))
+        n_lines = len(end_line.findall(text))
         print('Number of characters: {}, Number of words: {}, Number of lines:{} ' \
               .format(n_chars, n_words, n_lines))
         logging.info('Done')
@@ -79,13 +79,13 @@ def process(file_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=_DESCRIPTION)
-    parser.add_argument('-i', '--input', required=True, help='path to the input file')
-    parser.add_argument('-H', '--histogram', \
-                        help='Optional histogram of the frequencies: digit yes   if you want')
-    parser.add_argument('-s', '--skip', \
-                        help='Digit skip if you want to skip preamble and licence')
-    parser.add_argument('-S', '--statistic', \
-                        help='Digit yes if you want basic statistic')
+    parser.add_argument('input', help='path to the input file')
+    parser.add_argument('-H', '--histogram', action='store_true', \
+                        help='Optional histogram of the frequencies')
+    parser.add_argument('-s', '--skip', action='store_true', \
+                        help='Optional skip preamble and licence')
+    parser.add_argument('-S', '--statistic', action='store_true',  \
+                        help='Optional basic statistic')
     parser.add_argument('-l', '--logging_level', default='INFO', \
                         help='You can change the logging severity level')
     args = parser.parse_args()
